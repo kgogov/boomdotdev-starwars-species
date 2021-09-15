4b589f66a6abab56e051d3e9111146d7a8404107
Konstantin Gogov <konstantin.gogov@gmail.com>
2021-09-15 17:59:16 +0300

    test: attempt #3

--- src/app/Application.js
+++ src/app/Application.js
@@ -15,7 +15,9 @@ export default class Application extends EventEmitter {
     super();
 
     this.config = config;
-    this.data = {};
+    this.data = {
+      universe: new StarWarsUniverse()
+    };
 
     this.init();
   }
@@ -32,11 +34,6 @@ export default class Application extends EventEmitter {
    */
   async init() {
     // Initiate classes and wait for async operations here.
-    const universe = new StarWarsUniverse();
-    await universe.createSpecies();
-
-    this.data.universe = universe;
-
     this.emit(Application.events.APP_READY);
   }
 }
